7b3e4c7a080792f4ff19b1c427089dcdd29ee523
kaede <116987974+kaede4318@users.noreply.github.com>
2026-02-06 19:26:55 -0800

    fix bug where yt-dlp wasn't downloading some videos bc python wasn't using same env as shell

--- backend/downloader.py
+++ backend/downloader.py
@@ -1,33 +1,37 @@
 from subprocess import Popen, PIPE
 from pathlib import Path
-
+from shellwords import ShellWords
 import os
 
 CWD = os.getcwd()
+s = ShellWords()
+
+env = os.environ.copy()
+env["PATH"] = "/opt/homebrew/bin:" + env["PATH"]  # adjust if necessary
 
 # runs yt-dlp command
 def run(link=None, output_location="./output/"):
-    command = f'yt-dlp -P {output_location} -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4" {link}'
-    command = command.split(" ")
+    command = f'yt-dlp -P {output_location} -f "bv[vcodec^=avc1][ext=mp4]+ba[acodec^=mp4a][ext=m4a]/mp4" {link}'
+    command += ' --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"'
+    tokens = s.parse(command) # needed to remove quotes as shell does
 
     # stdout=PIPE ~ prints each line as it comes
-    p = Popen(
-        command, 
-        stdout=PIPE, 
-        stderr=PIPE,
-        text=True,
-        bufsize=1  # Line-buffered
-    )
+    p = Popen(tokens, stdout=PIPE, stderr=PIPE, text=True, bufsize=1, env=env)
 
+    # Read both stdout and stderr line by line
     for line in p.stdout:
-        print(line.strip())
+        print("STDOUT:", line.strip())
+    for line in p.stderr:
+        print("STDERR:", line.strip())
 
-    p.wait()  # Wait for process to complete
+    p.wait()
+    print("Process exited with code", p.returncode)
 
     # proc.terminate()  # politely stop it
     # proc.kill()       # forcefully stop it
 
 if __name__ == "__main__":
+    # testing
     INPUT = "https://www.youtube.com/watch?v=cTDK5Bluh5A" 
     out = "./output/"
-    run(link=INPUT, output_location=out, audio_only=False)
+    run(link=INPUT, output_location=out)
