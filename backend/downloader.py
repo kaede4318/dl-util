d315242ad22ad6f4030697626b9d3ba81eacf118
kaede <116987974+kaede4318@users.noreply.github.com>
2026-02-07 15:32:01 -0800

    add ability for run() to return filename as Path object

--- backend/downloader.py
+++ backend/downloader.py
@@ -1,6 +1,7 @@
 from subprocess import Popen, PIPE
 from pathlib import Path
 from shellwords import ShellWords
+import tempfile
 import os
 
 CWD = os.getcwd()
@@ -16,20 +17,25 @@ USER_AGENT = (
 )
 
 # runs yt-dlp command
-def run(link: str, output_location: Path="./output/") -> Path:
+def run(link: str) -> Path:
+    # Create a unique temporary file for this download
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_path = tmp_file.name
+        
     command = [
         "yt-dlp",
-        "-o", "/tmp/%(title)s - %(uploader)s [%(id)s].%(ext)s",
+        "-o", "/tmp/dl/%(title)s - %(uploader)s [%(id)s].%(ext)s",
         "-f", "bv[vcodec^=avc1][ext=mp4]+ba[acodec^=mp4a][ext=m4a]/mp4",
         "--user-agent", USER_AGENT,
         "--restrict-filenames",
         "--sleep-interval", "5", # sleep needed to avoid IP bans
         "--max-sleep-interval", "10", 
+        "--exec", f"echo {{}} > {tmp_path}", # parse this file to find filename
         link,
     ]
     # look if you are missing any commas ;)
     # btw, --restrict-filenames will get rid of non-ASCII characters like CJK characters
-    
+    # "--progress-template", "%(progress)j", # prints out progress as json (useful for parsing)
 
     p = Popen(
         command,
@@ -44,16 +50,20 @@ def run(link: str, output_location: Path="./output/") -> Path:
     for line in p.stdout:
         print("STDOUT:", line.strip())
     for line in p.stderr:
-        print("STDERR:", line.strip())
+        print(line.strip())
 
-    p.wait()
-    print("Process exited with code", p.returncode)
+    # Read the filename after download
+    with open(tmp_path, "r") as f:
+        final_filename = f.read().strip()
+    
+    os.unlink(tmp_path)
 
-    # proc.terminate()  # politely stop it
-    # proc.kill()       # forcefully stop it
+    return Path(final_filename)
+
 
 if __name__ == "__main__":
     # testing
     INPUT = "https://www.youtube.com/watch?v=cTDK5Bluh5A" 
+    jeffbeck = "https://www.youtube.com/watch?v=WNNkbE6EU3w" 
     out = "./output/"
-    run(link=INPUT, output_location=out)
+    run(link=jeffbeck)
